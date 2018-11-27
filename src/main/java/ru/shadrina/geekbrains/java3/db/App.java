dff417e3d4bf664f2e8f0a56eb7f0d1e44bb7b7b
oashvgr <oashadrina92@gmail.com>
2018-11-28 02:37:14 +0300

    lesson2 DataBase

--- src/main/java/ru/shadrina/geekbrains/java3/db/App.java
+++ src/main/java/ru/shadrina/geekbrains/java3/db/App.java
@@ -1,6 +1,5 @@
 package ru.shadrina.geekbrains.java3.db;
 
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import static ru.shadrina.geekbrains.java3.db.DataBase.*;
@@ -16,30 +15,9 @@ public class App {
             e.printStackTrace();
         }
 
-        try {
-            stmt.execute("CREATE TABLE IF NOT EXISTS goods (\n" +
-                    "    id     INTEGER PRIMARY KEY AUTOINCREMENT\n" +
-                    "                   NOT NULL,\n" +
-                    "    prodid STRING  UNIQUE,\n" +
-                    "    title  STRING,\n" +
-                    "    cost   INT\n" +
-                    ")");
-            System.out.println("Таблица создана");
-            stmt.execute("DELETE FROM goods");
-            PreparedStatement prstmt = connection.prepareStatement("INSERT INTO goods (prodid, title, cost) \n" +
-                    " VALUES (?, ?, ?)");
-            connection.setAutoCommit(false);
-            for (int i = 1; i <= 10000; i++) {
-                prstmt.setString(1, "" + i);
-                prstmt.setString(2, "товар" + i);
-                prstmt.setInt(3, 10 * i);
-                prstmt.addBatch();
-            }
-            prstmt.executeBatch();
-            connection.setAutoCommit(true);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        createTable();
+        clearTable();
+        fillInTable();
         Listener listener = new Listener();
         listener.run();
         disconnect();
--- src/main/java/ru/shadrina/geekbrains/java3/db/DataBase.java
+++ src/main/java/ru/shadrina/geekbrains/java3/db/DataBase.java
@@ -3,14 +3,12 @@ package ru.shadrina.geekbrains.java3.db;
 import java.sql.*;
 
 public class DataBase {
-    public static Statement stmt;
     public static Connection connection;
 
 
     public static void connect() throws ClassNotFoundException, SQLException {
         Class.forName("org.sqlite.JDBC");
         connection = DriverManager.getConnection("jdbc:sqlite:lesson2.db");
-        stmt = connection.createStatement();
     }
 
     public static void disconnect() {
@@ -21,10 +19,49 @@ public class DataBase {
         }
     }
 
+    public static void createTable() {
+        try (Statement stmt = connection.createStatement()) {
+            stmt.execute("CREATE TABLE IF NOT EXISTS goods (\n" +
+                    "    id     INTEGER PRIMARY KEY AUTOINCREMENT\n" +
+                    "                   NOT NULL,\n" +
+                    "    prodid STRING  UNIQUE,\n" +
+                    "    title  STRING,\n" +
+                    "    cost   INT\n" +
+                    ")");
+            System.out.println("Таблица создана");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void clearTable() {
+        try (Statement stmt = connection.createStatement()) {
+            stmt.execute("DELETE FROM goods");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void fillInTable() {
+        try (PreparedStatement prstmt = connection.prepareStatement("INSERT INTO goods (prodid, title, cost) \n" +
+                " VALUES (?, ?, ?)")) {
+            connection.setAutoCommit(false);
+            for (int i = 1; i <= 10000; i++) {
+                prstmt.setString(1, "" + i);
+                prstmt.setString(2, "товар" + i);
+                prstmt.setInt(3, 10 * i);
+                prstmt.addBatch();
+            }
+            prstmt.executeBatch();
+            connection.setAutoCommit(true);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void search(String goods) {
-        try {
-            String selectString = "SELECT cost FROM goods where title = " + "\"" + goods + "\"";
-            PreparedStatement prstmt = connection.prepareStatement(selectString);
+        try (PreparedStatement prstmt = connection.prepareStatement("SELECT cost FROM goods where title = ?")) {
+            prstmt.setString(1, goods);
             ResultSet rs = prstmt.executeQuery();
             if (rs.next()) {
                 System.out.println("Цена товара " + goods + ": " + rs.getString("cost"));
@@ -36,9 +73,9 @@ public class DataBase {
 
 
     public static void change(String goods, String cost) {
-        try {
-            String updateString = "UPDATE  goods SET cost = " + "\"" + cost + "\"" + " WHERE title= " + "\"" + goods + "\"";
-            PreparedStatement prstmt = connection.prepareStatement(updateString);
+        try (PreparedStatement prstmt = connection.prepareStatement("UPDATE  goods SET cost = ?  WHERE title= ?")) {
+            prstmt.setString(1, cost);
+            prstmt.setString(2, goods);
             if (prstmt.executeUpdate() > 0) System.out.println("Цена товара была успешно изменена");
             else System.out.println("Такого товара не обнаружено");
         } catch (SQLException e) {
@@ -48,19 +85,18 @@ public class DataBase {
 
 
     public static void sort(String min, String max) {
-        try {
-            String sortString = "SELECT * FROM goods WHERE  cost > " + "\"" + min + "\"" + " AND cost < " + "\"" + max + "\"" + " ORDER BY cost";
-            PreparedStatement prstmt = connection.prepareStatement(sortString);
+        try (PreparedStatement prstmt = connection.prepareStatement("SELECT * FROM goods " +
+                "WHERE  cost >= ? AND cost <= ? ORDER BY cost")) {
+            prstmt.setString(1, min);
+            prstmt.setString(2, max);
             ResultSet rs = prstmt.executeQuery();
-            if (!rs.next()) System.out.println("Товаров в этом ценовом диапазоне не обнаружено");
-            else {
-                while (rs.next()) {
-                    String prodid = rs.getString("prodid");
-                    String title = rs.getString("title");
-                    int cost = rs.getInt("cost");
-                    System.out.println(prodid + "\t" + title + "\t" + cost);
-                }
+            while (rs.next()) {
+                String prodid = rs.getString("prodid");
+                String title = rs.getString("title");
+                int cost = rs.getInt("cost");
+                System.out.println(prodid + "\t" + title + "\t" + cost);
             }
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
